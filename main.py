6b2fbc88e76769df0fb4289d63fa29e5c4f17ccd
lynda-trad <lynda.trad@laplateforme.io>
2021-10-05 14:32:01 +0200

    main.py : fonctions to check if adjacent tile was visited before

--- main.py
+++ main.py
@@ -14,6 +14,44 @@ def init_labyrinth():
 
 labyrinth = init_labyrinth()
 
+
+# Return True if tile was visited and False if not
+
+def upTile(tiles, i, j):
+    if i != 0:
+        if (i - 1, j) in tiles:
+            return True
+    else:
+        return False
+
+
+def downTile(tiles, i, j):
+    if i != len(labyrinth):
+        if (i + 1, j) in tiles:
+            return True
+        else:
+            return False
+
+
+def leftTile(tiles, i, j):
+    if j != 0:
+        if (i, j - 1) in tiles:
+            return True
+        else:
+            return False
+
+
+def rightTile(tiles, i, j):
+    if j != len(labyrinth):
+        if (i, j + 1) in tiles:
+            return True
+        else:
+            return False
+
+
+# List of visited tiles: exemple for tests
+tiles = {(0, 0), (1, 0)}
+
 filename = input("Entrez un nom de fichier\n")
 if not exists(filename):
     file = open(filename, "a+")
