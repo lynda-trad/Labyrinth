ac375658dfee468756fe56a6582944cd29f42f93
lynda-trad <lynda.trad@laplateforme.io>
2021-10-05 10:39:48 +0200

    main.py : labyrinth filled with # instead of blank spaces

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import string
 from os.path import exists
 import numpy as np
 
@@ -6,15 +5,9 @@ import numpy as np
 def init_labyrinth():
     num = int(input("Entrez la taille du labyrinthe\n"))
     size = num * 2 + 1
-    labyrinth = np.tile(' ', (size, size))
-    for i in range(size):
-        labyrinth[0][i] = '#'
-    for i in range(size):
-        labyrinth[i][0] = '#'
-    for i in range(size):
-        labyrinth[i][size - 1] = '#'
-    for i in range(size):
-        labyrinth[size - 1][i] = '#'
+    labyrinth = np.tile('#', (size, size))
+    # labyrinth[i][j] = ' '
+    # on creuse le passage petit a petit
     print(labyrinth, "\n")
     return labyrinth
 
