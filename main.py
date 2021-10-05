7cdc3a3cb3ab839000e298daef5b393f8d9601ba
lynda-trad <lynda.trad@laplateforme.io>
2021-10-05 10:33:19 +0200

    main.py : init labyrinth, walls done

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
+import string
 from os.path import exists
+import numpy as np
 
-size = int(input("Entrez la taille du labyrinthe"))
 
-filename = input("Entrez un nom de fichier")
+def init_labyrinth():
+    num = int(input("Entrez la taille du labyrinthe\n"))
+    size = num * 2 + 1
+    labyrinth = np.tile(' ', (size, size))
+    for i in range(size):
+        labyrinth[0][i] = '#'
+    for i in range(size):
+        labyrinth[i][0] = '#'
+    for i in range(size):
+        labyrinth[i][size - 1] = '#'
+    for i in range(size):
+        labyrinth[size - 1][i] = '#'
+    print(labyrinth, "\n")
+    return labyrinth
 
+
+labyrinth = init_labyrinth()
+
+filename = input("Entrez un nom de fichier\n")
 if not exists(filename):
     file = open(filename, "a+")
-    # generation de labyrinthe
-    file.close()
 else:
-    print("File already exists.")
+    file = open(filename, "w")
+
+labyrinth.tofile(file, '', '%s')
+file.close()
