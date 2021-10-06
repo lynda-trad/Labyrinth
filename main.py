104d0b1e9f966caa94a07d7615b278989530c29a
lynda-trad <lynda.trad@laplateforme.io>
2021-10-06 17:07:27 +0200

    main.py : add fonction initVisitedCells

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ def init_labyrinth():
     size = num * 2 + 1
     labyrinth = np.tile('#', (size, size))
 
-    # Beginning and end
     labyrinth[0][0] = ' '
     labyrinth[1][0] = ' '
     labyrinth[1][1] = ' '
@@ -16,17 +15,48 @@ def init_labyrinth():
     labyrinth[size - 2][size - 1] = ' '
     labyrinth[size - 2][size - 2] = ' '
 
-    # on creuse le passage petit a petit
     print(labyrinth, "\n")
     return labyrinth
+
+def initCells():
+    # Add walls to visited cells list
+    cells = {
+            (1, 1),
+            (mazeSize - 2, mazeSize - 2)
+            }
+
+    # line 0
+    for j in range(mazeSize):
+        if (0, j) not in cells:
+            cells.add((0, j))
+
+    # column 0
+    for i in range(mazeSize):
+        if (i, 0) not in cells:
+            cells.add((i, 0))
+
+    # line mazesize - 1
+    for j in range(mazeSize):
+        if (mazeSize - 1, j) not in cells:
+            cells.add((mazeSize - 1, j))
+
+    # column mazesize - 1
+    for i in range(mazeSize):
+        if (i, mazeSize - 1) not in cells:
+            cells.add((i, mazeSize - 1))
+
+    print(cells)
+    return cells
+
+# List of visited tiles: start and finish
 
 
 labyrinth = init_labyrinth()
 mazeSize = len(labyrinth)
+cells = initCells()
 
 
 # Return True if tile was visited and False if not
-
 def upCell(cells, i, j):
     if i != 0:
         if (i - 1, j) in cells:
@@ -63,6 +93,7 @@ def findUnvisitedCell(cells, i, j):
         if not upCell(cells, i, j):
             print("up")
             if i != 0:
+                print("return up")
                 return (i - 1, j)
             else:
                 findUnvisitedCell(cells, i, j)
@@ -73,7 +104,9 @@ def findUnvisitedCell(cells, i, j):
         if not downCell(cells, i, j):
             print("down")
             if i != mazeSize - 1:
-                return (i + 1, j)
+                print("return down")
+                i += 1
+                return (i, j)
             else:
                 findUnvisitedCell(cells, i, j)
         else:
@@ -83,6 +116,7 @@ def findUnvisitedCell(cells, i, j):
         if not leftCell(cells, i, j):
             print("left")
             if j != 0:
+                print("return left")
                 return (i, j - 1)
             else:
                 findUnvisitedCell(cells, i, j)
@@ -93,7 +127,9 @@ def findUnvisitedCell(cells, i, j):
         if not rightCell(cells, i, j):
             print("right")
             if j != mazeSize - 1:
-                return (i, j + 1)
+                print("return right")
+                j += 1
+                return (i, j)
             else:
                 findUnvisitedCell(cells, i, j)
         else:
@@ -109,14 +145,6 @@ def writeMazeToFile():
     labyrinth.tofile(file, '', '%s')
     file.close()
 
-
-# List of visited tiles: start and finish
-cells = {
-        (0, 0), (1, 0), (1, 1),
-        (mazeSize - 1, mazeSize - 1),
-        (mazeSize - 2, mazeSize - 2),
-        (mazeSize - 2, mazeSize - 1)
-        }
 
 # Test findUnvisitedCell with cell 0 0 and cell 1 0
 newCell = findUnvisitedCell(cells, 2, 0)
